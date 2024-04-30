095a2d17523ceaf98a092d896046f2a723869b88
Anson Tse <hiuyeungtse0@gmail.com>
2024-05-01 08:36:00 +1000

    link navigates from main page to create event page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from "next/link";
 import { SetStateAction, useState } from "react";
 
 export default function Home() {
@@ -18,6 +19,9 @@ export default function Home() {
 
   return (
     <div className="App mt-5 ml-5">
+      <Link key="create-event" href="event/create" className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        Create Event
+      </Link>
       <form onSubmit={handleSubmit} className="flex items-center">
         <label htmlFor="new-contestant-input">New Contestant</label>
         <input id="new-contestant-input" type="text" value={text} onChange={handleChange} />
